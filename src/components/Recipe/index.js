7716fdd301b4e79a7d9dc735a4d6f25dcb71ede9
Luiza Campos <luiza.almcampos@gmail.com>
2020-12-13 14:35:33 -0300

    atyle udate

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -5,13 +5,15 @@ import style from './recipe.module.css'
 export default function Recipe({ title, image, ingredients }) {
     return (
         <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ul className={style.list}>
-                {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
-            </ul>
-            <img className={style.image} src={image} />
+                <h1>{title}</h1>
+            <div className={style.container}>
+                <img className={style.image} src={image} />
+                <ul className={style.list}>
+                    {ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
